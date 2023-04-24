1446f0c09fe681aa7136dc4a1576c5cfd06c8379
Hasan AlMunshi <almunshi@sheridancollege.ca>
2023-04-23 20:12:28 -0400

    Created a Deck Class that creates an Array of cards and shuffles it

---
+++ src/blackjack/Deck.java
@@ -0,0 +1,34 @@
+package blackjack;
+
+/**
+ *
+ * @author hasanbasil
+ */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+    private final List<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<>();
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                cards.add(new Card(suit, rank));
+            }
+        }
+        shuffle();
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card dealCard() {
+        if (cards.isEmpty()) {
+            throw new IllegalStateException("Deck is empty");
+        }
+        return cards.remove(0);
+    }
+}
